42094224e1c78d9e83bd431e84719a1d76a07659
Torsten Albrecht <albrecht@pik-potsdam.de>
2023-07-05 13:30:08 +0200

    Some corrections

--- ex4/run_antarctica.sh
+++ ex4/run_antarctica.sh
@@ -9,8 +9,6 @@
 ###############################################################################
 
 PISM_PATH=$1
-MPIEXEC=$2
-PISM_SOURCE_DIR=$3
 
 ###############################################################################
 # bash run_antarctica.sh $PISMDIR 
@@ -20,43 +18,24 @@ set -e
 set -u
 set -x
 
-PISM_PATH=$1
-PISM_SOURCE_DIR=${PISM_PATH}
+# a coarse input file
+input_file=${PISM_PATH}/test/regression/pico_split/bedmap2_schmidtko14_50km.nc
 
-input_file=${PISM_SOURCE_DIR}/test/regression/pico_split/bedmap2_schmidtko14_50km.nc
-
-#temp_dir=$(mktemp -d -t pism_ant_pico_lc.XXXX) || exit 1
+# can be renamed
 temp_dir=results
 mkdir -p ${temp_dir}
-
 cp $input_file ${temp_dir}/input_01.nc
-
-#pushd ${temp_dir}
-
-cat > script.txt <<EOF
-mask=basins * 0;
-mask@units="";
-mask@flag_values="0, 2, 3, 4";
-mask@long_name="ice-type (ice-free/grounded/floating/ocean) integer mask";
-mask@pism_intent="diagnostic";
-
-where(thk >  topg / (-910.0 / 1028.0)) mask=2;
-where(thk <= topg / (-910.0 / 1028.0)) mask=3;
-where(thk == 0 && topg < 0) mask=4;
-EOF
-
-ncap2 -S script.txt -O ${temp_dir}/input_01.nc ${temp_dir}/input_01.nc
 
 # 50km
 grid="-bootstrap -Mx 120 -My 120 -Lz 6000 -Lbz 2000 -Mz 81 -Mbz 21"
 
 stressbalance="-pik -stress_balance ssa+sia -ssa_method fd"
-pico="-ocean pico -ocean_pico_file $input_file -gamma_T 1.0e-5 -overturning_coeff 0.8e6 -exclude_icerises -continental_shelf_depth -2500"
+pico="-ocean pico -ocean_pico_file ${temp_dir}/input_01.nc -gamma_T 1.0e-5 -overturning_coeff 0.8e6 -exclude_icerises -continental_shelf_depth -2500"
 surface="-atmosphere uniform -surface simple"   #"-surface pik"
 earth="-bed_def none" #lc
 
-
-mpiexec -n 2  ${PISM_PATH}/bin/pismr -verbose 2 -i ${temp_dir}/input_01.nc \
+# or srun, aprun, mpirun..
+${PISM_PATH}/bin/pismr -verbose 2 -i ${temp_dir}/input_01.nc \
               -config ${PISM_PATH}/share/pism/pism_config.nc \
               $grid \
               $stressbalance \
--- ex4/run_mismip2d.sh
+++ ex4/run_mismip2d.sh
@@ -6,58 +6,48 @@
 ###############################################################################
 # Test simulation of MISMIP2d case 
 # in GIA training school
+# check https://github.com/pism/pism/tree/dev/examples/mismip/mismip2d
 ###############################################################################
 
 PISM_PATH=$1
-MPIEXEC=$2
-PISM_SOURCE_DIR=$3
 
 ###############################################################################
 # bash run_mismip2d.sh $PISMDIR 
 ###############################################################################
+
 
 set -e
 set -u
 set -x
 
 PISM_PATH=$1
-PISM_SOURCE_DIR=${PISM_PATH}
 
-# check out: ${PISM_SOURCE_DIR}/examples/mismip/mismip2d/README.md
+export PATH=${PISM_PATH}/share/pism/examples/preprocessing/:$PATH
+export PYTHONPATH=${PISM_PATH}/share/pism/examples/preprocessing
 
-export PATH=${PISM_SOURCE_DIR}/share/pism/examples/preprocessing/:$PATH
-export PYTHONPATH=${PISM_SOURCE_DIR}/share/pism/examples/preprocessing
+# prepare 12km grid MISMIP setting
+cp ${PISM_PATH}/examples/mismip/mismip2d/run.py prepare_mismip.py
+python prepare_mismip.py -e 3a --mode=1 > experiment-3a-mode-1.sh
 
-# 12km grid
-cp ${PISM_SOURCE_DIR}/examples/mismip/mismip2d/run.py prepare_mismip.py
+# run first experiment
+bash experiment-3a-mode-1.sh 2 #>& out.3a-mode-1 &
 
-# --- a/examples/mismip/mismip2d/run.py
-#+++ b/examples/mismip/mismip2d/run.py
+# plot first result (or add to PATH variable)
+cp ${PISM_PATH}/examples/mismip/mismip2d/plot.py plot_mismip2d.py
+cp ${PISM_PATH}/examples/mismip/mismip2d/MISMIP.py ./
+python plot_mismip2d.py ABC1_3a_M1_A1.nc
+
+
+# if you want to run more experiments, you may insert a line in to prepare_mismip.py
+
 #@@ -104,6 +104,8 @@ class Experiment:
-#         "Options corresponding to modeling choices."
-#         config_filename = self.config(step)
 # 
 #+        boot_filename = "MISMIP_boot_%s_M%s_A%s.nc" % (self.experiment, self.mode, 1)
 #+
 #         options = ["-energy none",  # isothermal setup; allows selecting cold-mode flow laws
-#                    "-ssa_flow_law isothermal_glen",  # isothermal setup
-#                    "-yield_stress constant",
+
 #@@ -112,7 +114,7 @@ class Experiment:
-#                    "-gradient eta",
-#                    "-pseudo_plastic_q %e" % MISMIP.m(self.experiment),
-#                    "-pseudo_plastic_uthreshold %e" % MISMIP.secpera(),
 #-                   "-front_retreat_file %s" % input_file, # prescribe the maximum ice extent
 #+                   "-front_retreat_file %s" % boot_filename, # prescribe the maximum ice extent
-#                    "-config_override %s" % config_filename,
-#                    "-ssa_method fd",
-#                    "-cfbc",                # calving front boundary conditions
 
-
-python prepare_mismip.py -e 3a --mode=1 > experiment-3a-mode-1.sh
-
-bash experiment-3a-mode-1.sh 2 #>& out.3a-mode-1 &
-
-cp ${PISM_SOURCE_DIR}/examples/mismip/mismip2d/plot.py plot_mismip2d.py
-cp ${PISM_SOURCE_DIR}/examples/mismip/mismip2d/MISMIP.py ./
-python plot_mismip2d.py ABC1_3a_M1_A1.nc
 
